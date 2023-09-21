e2c308a10f565c71051695dda53505b8e3cde217
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-09-21 16:23:48 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -13,21 +13,62 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
+
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+
+    if animal:
+
+        animal_data = {
+            'id': animal.id,
+            'name': animal.name,
+            'species': animal.species,
+            'zookeeper_id': animal.zookeeper_id,
+            'enclosure_id': animal.enclosure_id
+        }
+        return make_response(animal_data, 200)
+    else:
+        return make_response({'message': 'Animal not found'}, 404)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+
+    if zookeeper:
+
+        zookeeper_data = {
+            'id': zookeeper.id,
+            'name': zookeeper.name,
+            'birthday': zookeeper.birthday,
+            'animals': [{'id': animal.id, 'name': animal.name} for animal in zookeeper.animals]
+        }
+        return make_response(zookeeper_data, 200)
+    else:
+        return make_response({'message': 'Zookeeper not found'}, 404)
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+
+    if enclosure:
+
+        enclosure_data = {
+            'id': enclosure.id,
+            'environment': enclosure.environment,
+            'open_to_visitors': enclosure.open_to_visitors,
+            'animals': [{'id': animal.id, 'name': animal.name} for animal in enclosure.animals]
+        }
+        return make_response(enclosure_data, 200)
+    else:
+        return make_response({'message': 'Enclosure not found'}, 404)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,17 +7,35 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date, nullable=True)
+
+    animals = db.relationship('Animal', backref='zookeeper', lazy=True)
+
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+
+    animals = db.relationship('Animal', backref='enclosure', lazy=True)
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey(
+        'zookeepers.id'), nullable=False)
+    enclosure_id = db.Column(db.Integer, db.ForeignKey(
+        'enclosures.id'), nullable=False)
